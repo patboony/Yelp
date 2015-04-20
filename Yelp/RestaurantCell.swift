ee9213abb68d51d65593274cf6ed28974460663b
patboony <pat@DN0a2332e0.SUNet>
2015-04-20 16:20:27 -0700

    First commit

---
+++ Yelp/RestaurantCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantCell.swift
+//  Yelp
+//
+//  Created by Pat Boonyarittipong on 4/20/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Yelp/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 9/19/14.
+//  Copyright (c) 2014 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var client: YelpClient!
+    
+    // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
+    let yelpConsumerKey = "EKOsTFalGLeKgSxdCCxacQ"
+    let yelpConsumerSecret = "xCwmQAUReBEMIcHVECFL9r6qzlE"
+    let yelpToken = "_uGGcFWzW_YQPz5zBY8tL-X3R2bsvUUL"
+    let yelpTokenSecret = "tAnH5CiC6-rgHEi2AyHDOYDyduA"
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+        
+        client.searchWithTerm("Thai", success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            println(response)
+        }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+            println(error)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
